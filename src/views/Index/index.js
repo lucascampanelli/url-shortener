b30e8cd0bdb224e065f1aa33eb20da9d1fe7991e
Lucas Campanelli <lucas.campanelli@outlook.com>
2020-08-26 04:41:25 -0300

    Design fix

--- src/views/Index/index.js
+++ src/views/Index/index.js
@@ -24,16 +24,19 @@ export default function Index(){
 
     return(
         <div className='container'>
+            <section className='titleSection'>
+                <h1 className='titleLabel'>URL Shortener</h1>
+                <h3 className='descriptionLabel'>Do more with short and recognizable URLs with one click</h3>
+            </section>
             <section className='urlSection'>
                 <form onSubmit={shorten}>
-                    <input value={url} onInput={e => {setUrl(e.target.value)}} type='text' name='url' className='url'/>
+                    <input value={url} onInput={e => {setUrl(e.target.value)}} type='text' name='url' className='url' placeholder='Paste your URL here'/>
                 </form>
             </section>
             <section className='shortenedSection'>
                 {
                     hash ? <div className='clipboard'><FiCopy className='copyIcon'/><input className='shortened' id='shortened' value={hash} onClick={copy}/></div> : ""
                 }
-                
             </section>
         </div>
     );
